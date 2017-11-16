e2d5ab18e049012e852074b3dc202ec3f68b638f
Glenn Friedman <glennfriedman@ool-ad0200f5.dyn.optonline.net>
2017-11-16 11:28:40 -0500

    fixed cross origin, rendering search results

--- fantasy_air/src/App.js
+++ fantasy_air/src/App.js
@@ -1,11 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+import Header from './Components/Header.js';
 
 class App extends Component {
+
+  constructor(props){
+    
+    super(props);
+
+    this.state = {
+      value: "",
+      searchResults: [],
+      url: "http://localhost:3000/players"
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.searchPlayers = this.searchPlayers.bind(this)
+  }
+
+  handleChange(event) {
+    event.preventDefault();
+    this.setState({
+      value: event.target.value
+    }, this.searchPlayers)
+  }
+
+  searchPlayers() {
+    if (this.state.value === "") {
+      return
+    }
+    else {
+    axios.get(`${this.state.url}/${this.state.value}`)
+    .then(res => {
+      this.setState({searchResults: res.data})
+      console.log('search results are ', res.data);
+      })
+    }
+  }
+
+  displayPlayerResults() {
+    let results = this.state.searchResults.search_results
+    let renderSearch = []
+    console.log(results)
+    if (this.state.searchResults.length === 0) {
+      return
+    }
+    else {
+      results.map(e => {
+        renderSearch.push(    
+          <p>{e[0]} {e[2]} {e[1]}</p>
+          )
+      })
+    return renderSearch
+    }
+  }
+
   render() {
     return (
-        <h1>This is Fantasy Air</h1>
+        <div>
+        <Header />
+          <div className="search-bar">
+            <input className='searchBar' type='text' ref={el=>{this.search=el}} placeholder="Search Players" onChange={this.handleChange} />
+        </div>
+        <div> {this.displayPlayerResults()} </div>
+        </div>
     );
   }
 }
---
+++ fantasy_air/src/Components/Header.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+// import { Link } from 'react-router-dom';
+
+class Header extends Component {
+
+	render(){
+		return(
+				<div className="header">
+					<div className="logo">
+						<h1>Fantasy Air</h1>
+						<h4>Fly Like a Champion</h4>
+					</div>
+					<div className="navBar">
+						<p></p>
+					</div>
+				</div>
+			)
+	}
+
+}
+
+
+export default Header;
