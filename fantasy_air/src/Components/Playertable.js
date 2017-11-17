292877e79b96319ad3dceae1a477a2a3c9fd8848
Glenn Friedman <glennfriedman@ool-ad0200f5.dyn.optonline.net>
2017-11-17 10:18:02 -0500

    added drop downs for position/player search - might lose react-bootstrap-table

--- fantasy_air/src/Components/Playertable.js
+++ fantasy_air/src/Components/Playertable.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 // import tablesort from 'tablesort';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 
+// this is doing something
+require('react-bootstrap-table/dist/react-bootstrap-table-all.min.css');
+// import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 
 class Playertable extends Component {
 
@@ -11,9 +14,12 @@ class Playertable extends Component {
 		
 		 this.state = {
 	     week: 10,
-	     position: '',
+	     position: 'All',
+	     playerData: []
     }
 		this.getPlayerData = this.getPlayerData.bind(this);
+		this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
 	}
 
 	componentDidMount() {
@@ -33,72 +39,72 @@ class Playertable extends Component {
 		)
 	}
 
-	// renderHeader() {
- //    return (
- //      <thead>
- //        <tr>
- //      		<th>Player</th>
- //          <th>Position</th>
- //          <th>Team</th>
- //          <th data-sort-method='number'>Receptions</th>
- //          <th data-sort-method='number'>Targets</th>
- //          <th data-sort-method='number'>Rec. Yards</th>
- //          <th data-sort-method='number'>YAC</th>
- //          <th data-sort-method='number'>Air Yards</th>
- //          <th data-sort-method='number'>AYPT</th>
- //          <th data-sort-method='number'>RACR</th>
- //          <th data-sort-method='number'>Team Attempts</th>
- //          <th data-sort-method='number'>Target Share</th>
- //          <th data-sort-method='number'>Team Air Yards</th>
- //          <th data-sort-method='number'>MS Air Yards</th>
- //          <th data-sort-method='number'>WOPR</th>
- //        </tr>
- //      </thead>
- //    )
- //  }
+	onChange(e) {
+		this.setState({
+      [e.target.name]: e.target.value
+		})
+	}
 
- //  renderTable() {
-
- //    const renderTable = [];
- //    this.state.playerData.map(e => {
- //      renderTable.push(
- //        <tr className={e.id} key={e.id}>
- //      		<td className='playerRow'>{e.full_name}</td>
- //      		<td className='playerRow'>{e.position}</td>
- //      		<td className='playerRow'>{e.team}</td>
- //      		<td className='playerRow'>{Number(e.rec)}</td>
- //      		<td className='playerRow'>{Number(e.tar)}</td>
- //      		<td className='playerRow'>{Number(e.rec_yards)}</td>
- //      		<td className='playerRow'>{Number(e.yac)}</td>
- //      		<td className='playerRow'>{Number(e.air_yards)}</td>
- //      		<td className='playerRow'>{Number(e.aypt)}</td>
- //      		<td className='playerRow'>{Number(e.racr)}</td>
- //      		<td className='playerRow'>{Number(e.tm_att)}</td>
- //      		<td className='playerRow'>{Number(e.target_share)}</td>
- //      		<td className='playerRow'>{Number(e.tm_airyards)}</td>
- //      		<td className='playerRow'>{Number(e.ms_air_yards)}</td>
- //      		<td className='playerRow'>{Number(e.wopr)}</td>
- //        </tr>
- //      )
- //      return renderTable;
- //    })
- //    return renderTable;
- //  }
+	onSubmit(event) {
+		event.preventDefault();
+		// /position/:position/:week
+		if (this.state.position === 'All') {
+			axios.get(`${this.props.url}/weeks/${this.state.week}`)
+				.then(res => {
+					console.log('res from submit weeks is ', res.data)
+					this.setState({playerData: res.data});
+			})
+		}
+		else {
+			axios.get(`${this.props.url}/position/${this.state.position}/${this.state.week}`)
+				.then(res => {
+					console.log('res from submit weeks with position is ', res.data)
+					this.setState({playerData: res.data});
+			})
+		}
+	}
 
 	render() {
 		// look into below later for player comparison - onSelect - compare players stats
 		// const selectRow = {
-  //   mode: 'checkbox',  // multi select
-  //   onSelect: this.handleRowSelect
-  // 	};
-		var selectRowProp = {
-  		mode: "checkbox",
-  		clickToSelect: true,
-  		bgColor: "rgb(238, 193, 213)" 
-		};
+  	//   mode: 'checkbox',  // multi select
+  	//   onSelect: this.handleRowSelect
+  	// 	};
+		// var selectRowProp = {
+  // 		mode: "checkbox",
+  // 		clickToSelect: true,
+  // 		bgColor: "rgb(238, 193, 213)" 
+		// };
 		return (
 			<div className="oneWeekData">
-      	<BootstrapTable data={this.state.playerData} selectRow={selectRowProp} exportCSV keyField='full_name' bordered={ true } >
+
+				<form className='viewSelect' onSubmit={this.onSubmit}>
+				<label htmlFor='positionSelect'>Position:</label>
+				<select className='select-values' id='positionSelect' name='position' value={this.state.position} onChange={this.onChange}>
+						<option defaultValue='all'>All</option>
+						 <option value='WR'>WR</option>
+						 <option value='TE'>TE</option>
+						 <option value='WR'>RB</option>
+				</select>
+				<label htmlFor='weekSelect'>Week:</label>
+				<select className='select-values' id='weekSelect' name='week' value={this.state.week} onChange={this.onChange}>
+								<option defaultValue='10'>10</option>
+								<option value='9'>9</option>
+								<option value='8'>8</option>
+								<option value='7'>7</option>
+								<option value='6'>6</option>
+								<option value='5'>5</option> 
+								<option value='4'>4</option>
+								<option value='3'>3</option>
+								<option value='2'>2</option>
+								<option value='1'>1</option>
+					</select>
+					<div className='submit'>
+					<input className="submitButton" type='submit' value='Submit' />
+					</div>
+				</form>
+
+      	<BootstrapTable data={this.state.playerData} exportCSV keyField='full_name' bordered={true} striped hover scroll>
           <TableHeaderColumn dataSort={ true } dataField='full_name'>Player Name</TableHeaderColumn>
           <TableHeaderColumn dataSort={ true } dataField='position'>Position</TableHeaderColumn>
           <TableHeaderColumn dataSort={ true } dataField='team'>Team</TableHeaderColumn>
@@ -118,7 +124,6 @@ class Playertable extends Component {
       </div>
 			)
 	}
-
 }
 
 export default Playertable;
