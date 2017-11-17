1543a4f283648ec4c78535c05f2b3ad1e8dda88c
Glenn Friedman <glennfriedman@ool-ad0200f5.dyn.optonline.net>
2017-11-17 12:10:15 -0500

    updated with react-table and auto weekly data update

--- fantasy_air/src/Components/Playertable.js
+++ fantasy_air/src/Components/Playertable.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 // import tablesort from 'tablesort';
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+// import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import ReactTable from 'react-table'
+import "react-table/react-table.css";
 
 // this is doing something
 require('react-bootstrap-table/dist/react-bootstrap-table-all.min.css');
@@ -13,28 +15,50 @@ class Playertable extends Component {
 		super(props)
 		
 		 this.state = {
-	     week: 10,
 	     position: 'All',
-	     playerData: []
+	     showWeek: '',
+	     playerData: [],
+	     weeks: [],
+	     week: 0
     }
 		this.getPlayerData = this.getPlayerData.bind(this);
+		this.getWeeks = this.getWeeks.bind(this);
 		this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+    this.renderWeeks = this.renderWeeks.bind(this);
 	}
 
 	componentDidMount() {
-		this.getPlayerData();
+		this.getWeeks();
+	}
+
+	getWeeks() {
+		axios.get(`${this.props.url}/showWeek`)
+		.then(res => {
+			// console.log('weeks are', res.data.weeks)
+			let current_week = Math.max(...res.data.weeks)
+			console.log('current_week is ', current_week)
+			this.setState({	week: current_week, weeks: res.data.weeks })
+			this.getPlayerData();
+		})
+	}
+
+	renderWeeks() {
+		const renderOptions = [];
+		this.state.weeks.map(e => {
+			renderOptions.push(
+					<option key={e} value={e}>{e}</option>
+				)
+			return renderOptions;
+		})
+		return renderOptions;
 	}
 
 	getPlayerData() {
 		console.log(this.props.url)
 		axios.get(`${this.props.url}/weeks/${this.state.week}`)
 		.then(res => {
-			this.setState({playerData: res.data}, 
-			 	() => {
-			 			console.log('state is ', this.state.playerData)
-            // tablesort(document.getElementById('myTable'));
-     			}) 
+			this.setState({playerData: res.data, showWeek: this.state.week})
 			}
 		)
 	}
@@ -52,29 +76,19 @@ class Playertable extends Component {
 			axios.get(`${this.props.url}/weeks/${this.state.week}`)
 				.then(res => {
 					console.log('res from submit weeks is ', res.data)
-					this.setState({playerData: res.data});
+					this.setState({playerData: res.data, showWeek: this.state.week});
 			})
 		}
 		else {
 			axios.get(`${this.props.url}/position/${this.state.position}/${this.state.week}`)
 				.then(res => {
 					console.log('res from submit weeks with position is ', res.data)
-					this.setState({playerData: res.data});
+					this.setState({playerData: res.data, showWeek: this.state.week});
 			})
 		}
 	}
 
 	render() {
-		// look into below later for player comparison - onSelect - compare players stats
-		// const selectRow = {
-  	//   mode: 'checkbox',  // multi select
-  	//   onSelect: this.handleRowSelect
-  	// 	};
-		// var selectRowProp = {
-  // 		mode: "checkbox",
-  // 		clickToSelect: true,
-  // 		bgColor: "rgb(238, 193, 213)" 
-		// };
 		return (
 			<div className="oneWeekData">
 
@@ -88,6 +102,14 @@ class Playertable extends Component {
 				</select>
 				<label htmlFor='weekSelect'>Week:</label>
 				<select className='select-values' id='weekSelect' name='week' value={this.state.week} onChange={this.onChange}>
+								{this.renderWeeks()}
+								{/*<option value='17'>17</option>
+								<option value='16'>16</option>
+								<option value='15'>15</option>
+								<option value='14'>14</option>
+								<option value='13'>13</option>
+								<option value='12'>12</option>
+								<option value='11'>11</option>
 								<option defaultValue='10'>10</option>
 								<option value='9'>9</option>
 								<option value='8'>8</option>
@@ -97,30 +119,87 @@ class Playertable extends Component {
 								<option value='4'>4</option>
 								<option value='3'>3</option>
 								<option value='2'>2</option>
-								<option value='1'>1</option>
+								<option value='1'>1</option>*/}
 					</select>
 					<div className='submit'>
 					<input className="submitButton" type='submit' value='Submit' />
 					</div>
 				</form>
 
-      	<BootstrapTable data={this.state.playerData} exportCSV keyField='full_name' bordered={true} striped hover scroll>
-          <TableHeaderColumn dataSort={ true } dataField='full_name'>Player Name</TableHeaderColumn>
-          <TableHeaderColumn dataSort={ true } dataField='position'>Position</TableHeaderColumn>
-          <TableHeaderColumn dataSort={ true } dataField='team'>Team</TableHeaderColumn>
-          <TableHeaderColumn dataSort={ true } dataField='rec'>Receptions</TableHeaderColumn>
-          <TableHeaderColumn dataSort={ true } dataField='tar'>Targets</TableHeaderColumn>
-          <TableHeaderColumn dataSort={ true } dataField='rec_yards'>Rec. Yards</TableHeaderColumn>
-          <TableHeaderColumn dataSort={ true } dataField='yac'>YAC</TableHeaderColumn>
-          <TableHeaderColumn dataSort={ true } dataField='air_yards'>Air Yards</TableHeaderColumn>
-          <TableHeaderColumn dataSort={ true } dataField='aypt'>AYPT</TableHeaderColumn>
-          <TableHeaderColumn dataSort={ true } dataField='racr'>RACR</TableHeaderColumn>
-          <TableHeaderColumn dataSort={ true } dataField='tm_att'>Team Attempts</TableHeaderColumn>
-          <TableHeaderColumn dataSort={ true } dataField='target_share'>Target Share</TableHeaderColumn>
-          <TableHeaderColumn dataSort={ true } dataField='tm_airyards'>Team Air Yards</TableHeaderColumn>
-          <TableHeaderColumn dataSort={ true } dataField='ms_air_yards'>MS Air Yards</TableHeaderColumn>
-          <TableHeaderColumn dataSort={ true } dataField='wopr'>WOPR</TableHeaderColumn>
-      	</BootstrapTable>
+				<div className='playerTable'>
+        <ReactTable
+          data={this.state.playerData}
+          columns={[
+            {
+              Header: `Air Yards Data for Week ${this.state.showWeek}`,
+              columns: [
+                {
+                  Header: "Full Name",
+                  accessor: "full_name",
+                  width: 150
+                },
+                {
+                  Header: "Position",
+                  accessor: "position"
+                },
+                {
+                  Header: "Team",
+                  accessor: "team"
+                },
+                {
+                	Header: "Receptions",
+                	accessor: "rec"
+                },
+                {
+                	Header: "Targets",
+                	accessor: "tar"
+                },
+                {
+                	Header: "Rec. Yards",
+                	accessor: "rec_yards"
+                },
+                {
+                	Header: "YAC",
+                	accessor: "yac"
+                },
+                {
+                	Header: "Air Yards",
+                	accessor: "air_yards"
+                },
+                {
+                	Header: "AYPT",
+                	accessor: "aypt"
+                },
+                {
+                	Header: "RACR",
+                	accessor: "racr"
+                },
+                {
+                	Header: "Team Attempts",
+                	accessor: "tm_att"
+                },
+                {
+                	Header: "Target Share",
+                	accessor: "target_share"
+                },
+                {
+                	Header: "Team Air Yards",
+                	accessor: "tm_airyards"
+                },
+                {
+                	Header: "MS Air Yards",
+                	accessor: "ms_air_yards"
+                },
+                {
+                	Header: "WOPR",
+                	accessor: "wopr"
+                },
+              ]
+            }
+          ]}
+          className="-striped -highlight"
+        />
+        </div>
       </div>
 			)
 	}
